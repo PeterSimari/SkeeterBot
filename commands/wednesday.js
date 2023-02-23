76977e1996738cf524be473f8af1797b4b8b93a8
Peter Simari <petersimari@gmail.com>
2023-02-23 00:07:17 -0500

    Adding gitignore and wednesday logging

--- commands/wednesday.js
+++ commands/wednesday.js
@@ -4,9 +4,13 @@ module.exports = function (msg) {
     var d = new Date();
     if (d.getDay() === 3) {
         if (msg.author.username === "loaf" || msg.author.username === "petis") {
-            console.log(`Wednesday triggered by ${msg.author.username}`);
+            logWednesday(msg);
             msg.channel.send(`https://media.tenor.com/ATYLM3TJsykAAAAC/ahhhh-its-wednesday-my-dudes.gif`);
             msg.channel.send(`https://gfycat.com/comfortablelamebangeltiger-bandwagon-normie-4chan-movie-shrek-dank`);
         }
     }
+}
+
+function logWednesday(msg) {
+    console.log(`${msg.author.username} triggered !wednesday`);
 }
